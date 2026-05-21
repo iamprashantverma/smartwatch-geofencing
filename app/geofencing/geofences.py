74143fc5480622284b34c8eea456d551c23e3295
iamprashantverma <prashant2k26@gmail.com>
2026-05-21 11:04:20 +0530

    feat: introduce geofence model for zone-based tracking and evaluation

---
+++ app/geofencing/geofences.py
@@ -0,0 +1,32 @@
+GEOFENCES = [
+    {
+        "name": "Office Zone",
+        "latitude": 28.6139,
+        "longitude": 77.2090,
+        "radius": 500 
+    },
+    {
+        "name": "Restricted Zone",
+        "latitude": 28.6120,
+        "longitude": 77.2105,
+        "radius": 200
+    },
+    {
+        "name": "Warehouse Zone",
+        "latitude": 28.6155,
+        "longitude": 77.2080,
+        "radius": 300
+    },
+    {
+        "name": "Research Lab Zone",
+        "latitude": 28.6148,
+        "longitude": 77.2120,
+        "radius": 250
+    },
+    {
+        "name": "Parking Zone",
+        "latitude": 28.6115,
+        "longitude": 77.2075,
+        "radius": 400
+    }
+]
