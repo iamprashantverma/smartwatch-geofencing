9b31bacdd58827ce02c775368ae42c203c06bd96
iamprashantverma <prashant2k26@gmail.com>
2026-05-21 16:49:42 +0530

    chore:add ollama endpoint and model configurations in config

--- app/core/config.py
+++ app/core/config.py
@@ -4,14 +4,17 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    
+    OLLAMA_BASE_MODEL: str
+    OLLAMA_BASE_URL: str
+    AI_EXPLANATION:bool
+
     class Config:
         env_file = ".env"
         case_sensitive = True
 
 
-@lru_cache
-def get_settings():
+@lru_cache()
+def get_settings() -> Settings:
     return Settings()
 
 
