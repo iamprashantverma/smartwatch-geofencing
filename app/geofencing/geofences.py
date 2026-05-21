777c1a529faf8aadb6766135885540f22cbcb96e
iamprashantverma <prashant2k26@gmail.com>
2026-05-21 15:37:38 +0530

    refactor: removed intercating circle zone

--- app/geofencing/geofences.py
+++ app/geofencing/geofences.py
@@ -3,30 +3,30 @@ GEOFENCES = [
         "name": "Office Zone",
         "latitude": 28.6139,
         "longitude": 77.2090,
-        "radius": 500 
+        "radius": 300
     },
     {
         "name": "Restricted Zone",
-        "latitude": 28.6120,
-        "longitude": 77.2105,
+        "latitude": 28.6170,
+        "longitude": 77.2150,
         "radius": 200
     },
     {
         "name": "Warehouse Zone",
-        "latitude": 28.6155,
-        "longitude": 77.2080,
-        "radius": 300
-    },
-    {
-        "name": "Research Lab Zone",
-        "latitude": 28.6148,
-        "longitude": 77.2120,
+        "latitude": 28.6085,
+        "longitude": 77.2030,
         "radius": 250
     },
     {
+        "name": "Research Lab Zone",
+        "latitude": 28.6200,
+        "longitude": 77.2050,
+        "radius": 220
+    },
+    {
         "name": "Parking Zone",
-        "latitude": 28.6115,
-        "longitude": 77.2075,
-        "radius": 400
+        "latitude": 28.6050,
+        "longitude": 77.2125,
+        "radius": 180
     }
 ]
